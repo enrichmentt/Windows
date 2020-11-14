1c04a3a5b33f73ca80f90fb60a3e01302b22734a
Vladimir P <enrichment.off@gmail.com>
2020-11-14 14:25:29 +0300

    Add path

--- Reset-TrialTermSrv/Reset-TrialTermSrv.ps1
+++ Reset-TrialTermSrv/Reset-TrialTermSrv.ps1
@@ -1,10 +1,11 @@
 ﻿$user = $env:USERNAME
+$PathAclExe = "C:\windows\temp\SetACL.exe"
 
-cmd /c "SetACL.exe -on `"hklm\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod`" -ot reg -actn setowner -ownr `"n:$user`""
+cmd /c "$PathAclExe -on `"hklm\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod`" -ot reg -actn setowner -ownr `"n:$user`""
 
 if (($user -notlike "*SYSTEM*") -or ($user -notlike "*ALL APPLICATION PACKAGE*") -or ($user -notlike "*NETWORK SERVICE*"))
 {
-    cmd /c "SetACL.exe -on `"hklm\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod`" -ot reg -actn ace -ace `"n:$user;p:full`""
+    cmd /c "$PathAclExe -on `"hklm\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod`" -ot reg -actn ace -ace `"n:$user;p:full`""
 
     $acl = Get-Acl 'hklm:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod'
     $rule = New-Object System.Security.AccessControl.RegistryAccessRule ("$user", "FullControl", "Allow")
@@ -14,4 +15,4 @@ if (($user -notlike "*SYSTEM*") -or ($user -notlike "*ALL APPLICATION PACKAGE*")
 
 Remove-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod' -Name 'L$RTMTIMEBOMB_1320153D-8DA3-4e8e-B27B-0D888223A588'
 
-cmd /c 'SetACL.exe -on "hklm\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" -ot reg -actn setowner -ownr "n:NETWORK SERVICE"'
+cmd /c "$PathAclExe -on "hklm\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" -ot reg -actn setowner -ownr `"n:NETWORK SERVICE`""
